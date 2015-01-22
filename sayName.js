bfb15274e6b65af20f85ce0a444cbd08f149f339
Bryan Brinton <bryan@sevenpeaks.com>
2015-01-21 19:58:59 -0700

    final submission

--- sayName.js
+++ sayName.js
@@ -2,12 +2,23 @@
 
   //code here
 
+var Person = function(name, age) {
+	this.name = name;
+	this.age = age;
+}
 
 //Now create three instances of Person with data you make up
 
   //code here
 
+var user1 = new Person("Bryan", 33);
+var user2 = new Person("Robert", 27);
+var user3 = new Person("Drew", 4);
 
 //Now add a sayName method on your Person class that will alert the name of whatever Person instance called it.
 
   //code here
+
+Person.prototype.sayName = function(){
+	alert(this.name);
+}
